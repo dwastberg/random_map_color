c0d411ef3b6857c89fb3b00d863897008d6c3560
Dag <dwastberg@gmail.com>
2020-04-20 15:11:38 +0200

    validate input

--- random_map_colors.py
+++ random_map_colors.py
@@ -20,13 +20,28 @@ def get_random_colors(n, h_range=(0.0, 1.0), s_range=(0.2, 1.0), v_range=(0.6, 1
     rbg_int: a list of integer value RGB tuples
     """
 
+    assert (
+        h_range[0] >= 0 and h_range[1] <= 1
+    ), "h_range min and max must be between 0-1"
+    assert (
+        s_range[0] >= 0 and s_range[1] <= 1
+    ), "s_range min and max must be between 0-1"
+    assert (
+        v_range[0] >= 0 and v_range[1] <= 1
+    ), "v_range min and max must be between 0-1"
+
+    assert h_range[0] < h_range[1], "h_range min must be less than h_range max"
+    assert s_range[0] < s_range[1], "s_range min must be less than s_range max"
+    assert v_range[0] < v_range[1], "v_range min must be less than v_range max"
+
     sample_space = [
         (random.uniform(*h_range), random.uniform(*s_range), random.uniform(*v_range))
         for r in range(10 * n)
     ]
     selected_colors = []
     selected_colors.append(sample_space.pop())
-    # find the color in our sample space that maximizes the minimum distance too all the already select colors.
+    # find the color in our sample space that maximizes the minimum 
+    # distance too all the already select colors.
     for i in range(n - 1):
         min_dist = [_min_dist_to_selected(selected_colors, c) for c in sample_space]
         next_color_idx = min_dist.index(max(min_dist))
